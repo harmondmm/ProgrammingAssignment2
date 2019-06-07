5293795a4938d3ce1371b2e511eaeed2b5bb6734
harmondmm <50682608+harmondmm@users.noreply.github.com>
2019-06-07 16:16:03 -0500

    Add files via upload
    Peer-graded Assignment: Programming Assignment 2: Lexical Scoping.  This program contains a pair of functions that cache the inverse of a matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Peer-graded Assignment: Programming Assignment 2: Lexical Scoping
 
-## Write a short comment describing this function
+## Matrix inversion is usually a costly computation and there may be some benefit to 
+## caching the inverse of a matrix rather than computing it repeatedly
+## This program contains a pair of functions that cache the inverse of a matrix
+
+## This function creates a special "matrix" object that can cache its inverse.
+
+x <- rbind(c(10,1), c(1,10))
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    solx <- NULL
+    set <- function(y) {
+        x <<- y
+        solx <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) solx <<- solve
+    getsolve <- function() solx
+    list(set = set, get = get, setsolve = setsolve, getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by 
+## makeCacheMatrix above. If the inverse has already been calculated (and the matrix 
+## has not changed), then cacheSolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    
+    # Return a matrix that is the inverse of 'x'
+    solx <- x$getsolve()
+    
+    if (!is.null(solx)) {
+        message("Cached data retrieved")
+        return(solx)
+    }
+    data <- x$get()
+    solx <- solve(data)
+    x$setsolve(solx)
+    solx
 }
